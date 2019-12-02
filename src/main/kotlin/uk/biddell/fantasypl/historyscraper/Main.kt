e913468d350269137c0501b73602665ab825e834
Luke Biddell <luke.a.biddell@gmail.com>
2019-12-02 17:50:12 +0000

    Improve performance by reducing number of calls.

--- src/main/kotlin/uk/biddell/fantasypl/historyscraper/Main.kt
+++ src/main/kotlin/uk/biddell/fantasypl/historyscraper/Main.kt
@@ -4,21 +4,18 @@ import org.openqa.selenium.By
 import org.openqa.selenium.OutputType
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.support.ui.ExpectedConditions
-import org.openqa.selenium.support.ui.WebDriverWait
-import uk.biddell.fantasypl.extension.findElementSafe
 import java.io.File
 import java.util.concurrent.TimeUnit
 import javax.imageio.ImageIO
 import kotlin.math.roundToInt
 
-private const val timeout = 3L
+private const val timeout = 10L
 private const val zoom = 1.0
 private const val width = 1800
 private const val height = 1850
 
-private val byTeam: By = By.cssSelector("div[class='sc-bdVaJa elkxqB']")
-private val byNext: By =
+private val byTeam = By.cssSelector("div[class='sc-bdVaJa elkxqB']")
+private val byNext =
     By.cssSelector("div[class='Pager__PagerButton-s2eddx-3 Pager__PagerButtonNext-s2eddx-4 kCqZVW']")
 
 private fun zoom(x: Int) = (x * zoom).roundToInt()
@@ -34,24 +31,21 @@ fun start(teamID: Int) = ChromeDriver(ChromeOptions().apply {
 }).run {
     manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS)
 
-    var i = 1
-
-    get("https://fantasy.premierleague.com/entry/$teamID/event/$i")
+    get("https://fantasy.premierleague.com/entry/$teamID/event/1")
 
     do {
-        println(currentUrl)
+        val gameweek = with(currentUrl) { substring(this.lastIndexOf('/') + 1) }
+        println(gameweek)
         val finalGameweek = findElements(byNext).size < 2
-        screenshotTeam(File("img/${teamID}_${i++}.png"))
-        findElementSafe(byNext)?.click()
+        screenshotTeam(File("img/${teamID}_${gameweek}.png"))
+        findElement(byNext).click()
     } while (!finalGameweek)
 }
 
 private fun ChromeDriver.screenshotTeam(dest: File) {
     executeScript("document.body.style.zoom = '$zoom'")
 
-    val ele = WebDriverWait(this, timeout)
-        .until(ExpectedConditions.visibilityOfElementLocated(byTeam))
-
+    val ele = findElement(byTeam)
     val screenshot = getScreenshotAs(OutputType.FILE)
 
     val point = ele.location
